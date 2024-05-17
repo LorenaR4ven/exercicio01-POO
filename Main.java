3bcc0fc89a0dbbf579f63cc391d8d97264a0c4fa
LorenaR4ven <129565052+LorenaR4ven@users.noreply.github.com>
2024-05-16 22:41:30 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package exercicio01;
+
+public class Main {
+    public static void main(String[] args) {
+        Progressões a = new Progressões();
+        Fibonacci f = new Fibonacci();
+        Geometrica g =new Geometrica();
+
+        
+        a.Progressão(6, 5);
+        f.Progressão(6, 5);
+        g.Progressão(6, 5);
+    }
+}
